50fe882d0b9530e046cd2ed55c35d6b3304374cd
devFendrix <dev.fendrix@gmail.com>
2023-06-16 23:00:40 +0200

    add player

--- src/game/player.ts
+++ src/game/player.ts
@@ -1,1 +1,38 @@
-// Player
+/**
+ * Class player
+ *
+ * Cette classe représente le modèle d'un joueur.
+ * Elle fournit des opérations pour manipuler un joueur, son pari, et ses gains.
+ *
+ * @version : 0.01
+ * */
+
+import { BetOption } from "../types.js";
+
+class Player {
+    private bet: number = 0;
+    private betOption: BetOption;
+
+    public getBetOption(): BetOption {
+        return this.betOption;
+    }
+
+    public getBet(): Number {
+        return this.bet;
+    }
+
+    public setBet(bet: Number): void {
+        this.bet = bet;
+    }
+
+    public gain(bet: Number): Number {
+        if(bet < 5){
+            return bet - 1;
+        } else {
+            return bet * .9;
+        }
+    }
+
+}
+
+export default Player;
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,8 @@ import Card from "./game/card.js"
 
 export type CardValue = Record<string, number>;
 export type CardTuple = [Card?, Card?, Card?];
+export enum BetOption {
+    Banker = 'Banker',
+    Player = 'Player',
+    Tie = 'Tie'
+}
