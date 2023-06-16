0f07222a728446a580119c846b779ad5e72e98d1
Aarnow <marcweb.developer@gmail.com>
2023-06-16 13:12:02 +0200

    feat: add Card class

--- dist/game/card.js
+++ dist/game/card.js
@@ -1,2 +1,12 @@
-"use strict";
-// Card
+export class Card {
+    constructor(value, suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+    getValue() {
+        return this.value;
+    }
+    getSuit() {
+        return this.suit;
+    }
+}
--- src/game/card.ts
+++ src/game/card.ts
@@ -1,1 +1,17 @@
-// Card
+export class Card {
+    private value: string;
+    private suit: string;
+
+    constructor(value: string, suit: string) {
+        this.value = value;
+        this.suit = suit;
+    }
+
+    public getValue(): string {
+        return this.value;
+    }
+
+    public getSuit(): string {
+        return this.suit;
+    }
+}
