1dd1fe215018ad7b1d65cb1b46b7d1dd4cbd9e54
Aarnow <marcweb.developer@gmail.com>
2023-06-17 01:50:22 +0200

    feat: add baccaratGame

---
+++ src/game/baccaratGame.ts
@@ -0,0 +1,29 @@
+/**
+ * Class Baccarat
+ *
+ * Cette classe représente le modèle d'une partie de Baccarat.
+ * Elle fournit des opérations pour manipuler :
+ *      - des joueurs, leur paris, et leurs gains/pertes.
+ *
+ * @version : 0.01
+ * */
+
+import Player from "./player.js"
+import Deck from "./deck.js"
+import Hand from "./hand.js"
+
+class BaccaratGame {
+    private puntos: Player[];
+    private deck: Deck;
+    private playerHand: Hand;
+    private bankerHand: Hand;
+
+    constructor(puntos: Player[], deck: Deck, playerHand: Hand, bankerHand: Hand){
+        this.puntos = [];
+        this.deck = new Deck();
+        this.playerHand = new Hand();
+        this.bankerHand = new Hand();
+    }
+}
+
+export default BaccaratGame;
