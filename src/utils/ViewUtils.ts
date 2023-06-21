1f2403b99cf6f8eb5e5c9ba49cd5ea1bdd167e0f
devFendrix <dev.fendrix@gmail.com>
2023-06-21 21:18:46 +0200

    fix error in start

--- src/utils/ViewUtils.ts
+++ src/utils/ViewUtils.ts
@@ -1,37 +1,38 @@
 import Player from "../game/player";
 
-export let removePlayerView = (player: Element) => {
-    player.querySelectorAll('.player-bet-option').forEach((betOption) => {
-        betOption.classList.remove('active');
-        betOption.classList.add('unactive');
-    });
-    player.querySelector('.amount').textContent = "-";
-    player.querySelector('.amount-total').textContent = "-";
-    player.querySelector('.victory').textContent = "-";
-    player.querySelector('.defeat').textContent = "-";
-    player.querySelector('.percent').textContent = "- %";
-    player.querySelector('.input-bet').value = "";
-};
-
 export let addLineGameView = (text: string) => {
     let containerGamaManager = document.querySelector('.container-game-manager');
     let line = document.createElement('div');
+
     line.className = 'table-line';
     line.textContent = text;
-    containerGamaManager.parentNode.insertBefore(line, containerGamaManager);
+    
+    if(containerGamaManager && containerGamaManager.parentNode){
+        containerGamaManager.parentNode.insertBefore(line, containerGamaManager);
+    }
 };
 
 export let updateStatisticsView = (container: Element, player: Player) => {
+    const amountElem = container.querySelector('.amount');
+    const amountTotalElem = container.querySelector('.amount-total');
+    const victoryElem = container.querySelector('.victory');
+    const defeatElem = container.querySelector('.defeat');
+    const percentElem = container.querySelector('.percent');
+
+
     container.querySelectorAll('.player-bet-option').forEach((betOption) => {
         betOption.classList.remove('active');
         betOption.classList.add('unactive');
     });
-    container.querySelector('.amount').textContent = player.getAmount().toString();
-    container.querySelector('.amount-total').textContent = player.getStatistics().earning.toString();
-    container.querySelector('.victory').textContent = player.getStatistics().wins.toString();
-    container.querySelector('.defeat').textContent = player.getStatistics().losses.toString();
-    container.querySelector('.percent').textContent = player.getStatistics().winningPercentage.toString();
-    container.querySelector('.input-bet').value = "";
+
+    if(amountElem) amountElem.textContent = player.getAmount().toString();
+    if(amountTotalElem) amountTotalElem.textContent = player.getStatistics().earning.toString();
+    if(victoryElem) victoryElem.textContent = player.getStatistics().wins.toString();
+    if(defeatElem) defeatElem.textContent = player.getStatistics().losses.toString();
+    if(percentElem) percentElem.textContent = player.getStatistics().winningPercentage.toString();
+
+    let input = container.querySelector('.input-bet') as HTMLInputElement;
+    input.value = "";
 }
 
 export const deleteAllLogs = () => {
